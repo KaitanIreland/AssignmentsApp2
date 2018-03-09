3fd2544a7c79344dda2ce059f59f87133844b401
KaitanKale <kaitan.ireland98@gmail.com>
2018-03-08 23:41:51 -0600

    Pushing more of the problems, some errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public Main() throws FileNotFoundException {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //Output the current date-time
         LocalDateTime today = LocalDateTime.now();
         System.out.println("The date and time right now is " + today + ".");
@@ -29,7 +29,7 @@ public class Main {
         System.out.println(fiveWeeks);
 
         //Initialize a LocalDateTime object to your birthdate and the time 12:35 PM.
-        LocalDateTime kaitansBirthday = LocalDateTime.of(1998, 10 , 14,  12,  35 );
+        LocalDateTime kaitansBirthday = LocalDateTime.of(1998, 10, 14, 12, 35);
 
         //Output the day of the week (Sunday-Saturday that you were born
         DayOfWeek dayofWeek = kaitansBirthday.getDayOfWeek();
@@ -54,50 +54,39 @@ public class Main {
         //Given two dates, output the earlier..
         if (randomDate1.isBefore(randomDate2)) {
             System.out.println("The earlier date is " + randomDate1);
-        }
-        else
+        } else
             System.out.println("The earlier date is " + randomDate2);
 
         //Create a file with 100 random "month/day/year hour:minutes" (in that format) on each line.
-        File outfile = new File( "RandomDateFile.txt" );
-                    try( PrintWriter pw = new PrintWriter(outfile) ) {
-                        for (int i = 0; i < 50; i++) {
-                            LocalDateTime randomDatesLoop = today.minusSeconds(rand.nextInt(345678909));
-                            LocalDateTime randomDatesLoop2 = today.plusSeconds(rand.nextInt(345678909));
-                            DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MM-dd-yy HH:mm");
-                            pw.println(randomDatesLoop.format(formatter2));
-                            pw.println(randomDatesLoop2.format(formatter2));
-                        }
-                           } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
+        File outfile = new File("RandomDateFile.txt");
+        try (PrintWriter pw = new PrintWriter(outfile)) {
+            for (int i = 0; i < 50; i++) {
+                LocalDateTime randomDatesLoop = today.minusSeconds(rand.nextInt(345678909));
+                LocalDateTime randomDatesLoop2 = today.plusSeconds(rand.nextInt(345678909));
+                DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm");
+                pw.println(randomDatesLoop.format(formatter2));
+                pw.println(randomDatesLoop2.format(formatter2));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
         //TODO Store data from the file into an ArrayList of LocalDateTime objects.
-        //ArrayList<LocalDateTime> dataFromFile = new ArrayList<LocalDateTime>();
-    File infile = new File( "RandomDateFile.txt" );
-
-    try( Scanner sc = new Scanner(infile)) {
-            while( sc.hasNext() ) {
-
-            }
-        }
-         catch (FileNotFoundException e) {
-        e.printStackTrace();
-    }
+        ArrayList<LocalDateTime> dataFromFile = readDataFromFile("RandomDateFile.txt");
+        System.out.println(dataFromFile);
 
 
         //TODO Output the number of stored dates in the year [Y]
         Scanner scIn = new Scanner(System.in);
         System.out.println("Which year would you like to investigate?");
-        int investigatedYear = scIn.nextInt(9999) + 1;
+        int investigatedYear = scIn.nextInt();
         if (investigatedYear > 9999) {
             System.out.println("Sorry, our software does not support years that far into the future.");
         }
-        if (investigatedYear > 0) {
+        if (investigatedYear < 0) {
             System.out.println("Sorry, our software does not support years that far in the past.");
         }
-
 
 
         //TODO Count the number of stored dates within the current year
@@ -105,14 +94,19 @@ public class Main {
 
 
         //TODO Count the number of duplicates
+        for (int i = 0; i < dataFromFile.size(); i++) {
+            if (dataFromFile.get(i).equals(dataFromFile.get())) {
+
+            }
+        }
 
         //TODO Sort the dates in chronological order
+        for (int i = 0; i < dataFromFile.size(); i++) {
+            if (dataFromFile.get(i+1).isAfter(dataFromFile.get(i))) {
 
-        //TODO Count the number of stored dates in the current year
+            }
 
-        //TODO Count the number of duplicates
-
-        //TODO Sort the dates in chronological order
+        }
 
         //TODO Count the number of duplicates in a sorted list without using a Java Set.
 
@@ -125,5 +119,24 @@ public class Main {
         //TODO Determine the indexes of the elements that have the earliest starting time, regardless of date.
 
         //TODO Output a date in the format "January 1st, 2018
-    }}
+    }
+
+    private static ArrayList<LocalDateTime> readDataFromFile(String s) throws FileNotFoundException {
+        Scanner scKeyboard = new Scanner(System.in); // System.in means read from the keyboard
+        Scanner sc = new Scanner(new File("RandomDateFile.txt"));  // means read from the given filename
+        while (sc.hasNext()) {
+            String aLineOfInput = sc.nextLine();  // read a line from sc
+            String[] partDateFromTime = aLineOfInput.split(" "); // split the line on the space
+            System.out.println("time part is " + partDateFromTime[1] + " and date part is " + partDateFromTime[0]);
+
+            // now split the 3 parts of the date based on "/"
+
+            // now split the  2 parts of the time based on ":"
+            String[] partTime = partDateFromTime[1].split(":");
+            System.out.println("mm is " + partTime[1] + " and hh is " + partTime[0]);
+            ArrayList<LocalDateTime> ar = new ArrayList<>();
+        }
+        return ar;
+    }
+}
 
